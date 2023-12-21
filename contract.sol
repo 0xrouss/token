69be52bbe491086f12cc6025409b9800a1338393
0xrouss <pablovigara@gmail.com>
2023-12-21 14:36:46 +0100

    airdrop

--- contract.sol
+++ contract.sol
@@ -5,38 +5,16 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Token is ERC20, ERC20Burnable, Ownable {
-    mapping(address => bool) public whitelistedWallets;
-
-    constructor(
-        string memory _name,
-        string memory _symbol,
-        uint256 _totalSupply
-    ) ERC20(_name, _symbol) Ownable(msg.sender) {
-        _mint(msg.sender, _totalSupply);
-    }
-
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
-    }
-
-    function addToWhitelist(address wallet) external onlyOwner {
-        whitelistedWallets[wallet] = true;
-    }
-
-    function removeFromWhitelist(address wallet) external onlyOwner {
-        whitelistedWallets[wallet] = false;
-    }
-
-    function isWhitelisted(address wallet) external view returns (bool) {
-        return whitelistedWallets[wallet];
+contract McRib10x is ERC20, ERC20Burnable, Ownable {
+    constructor() ERC20("McRib10x", "MR10X") Ownable(msg.sender) {
+        _mint(msg.sender, 42069069420 * 10 ** decimals());
     }
 
     function transfer(
         address recipient,
         uint256 amount
     ) public override returns (bool) {
-        if (!whitelistedWallets[msg.sender]) {
+        if (msg.sender != owner()) {
             require(
                 amount <= (totalSupply() * 5) / 1000,
                 "Transfer amount must be less than 0.5% of total supply"
@@ -50,12 +28,26 @@ contract Token is ERC20, ERC20Burnable, Ownable {
         address recipient,
         uint256 amount
     ) public override returns (bool) {
-        if (!whitelistedWallets[sender]) {
+        if (msg.sender != owner()) {
             require(
                 amount <= (totalSupply() * 5) / 1000,
                 "Transfer amount must be less than 0.5% of total supply"
             );
         }
         return super.transferFrom(sender, recipient, amount);
+    }
+
+    function airdrop(
+        address[] memory recipients,
+        uint256[] memory amounts
+    ) external onlyOwner {
+        require(
+            recipients.length == amounts.length,
+            "Mismatched array lengths"
+        );
+
+        for (uint256 i = 0; i < recipients.length; i++) {
+            transfer(recipients[i], amounts[i]);
+        }
     }
 }
